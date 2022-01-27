f9b1ccfe1055d14cdaa3566a034fbb24346c34e3
David Read <davidread1998@gmail.com>
2022-01-27 18:40:38 -0500

    Redo ColorActivity layout with MaterialButton views to be more consistent with rest of app
    activity_color.xml:
    - Remove RadioGroup and RadioButton views.
    - Add a MaterialButton view for each selectable color.
    
    ColorActivity.java:
    - Finally document this activity.
    - Swap out RadioButton references with MaterialButton references.
    - Use the icon attribute of MaterialButton to represent a selected color.
    
    drawable:
    - Add "ic_radio_button_checked" drawable to represent a selected color.
    - Add "ic_radio_button_unchecked" drawable to represent an unselected color.
    
    strings.xml:
    - Add string resources for the MaterialButton views in ColorActivity.
    
    themes.xml:
    - Remove redundant attributes from GameOptionButton style.

--- app/src/main/java/com/davidread/lightsout/ColorActivity.java
+++ app/src/main/java/com/davidread/lightsout/ColorActivity.java
@@ -4,37 +4,54 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.RadioButton;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.content.res.AppCompatResources;
 
+import com.google.android.material.button.MaterialButton;
+
+/**
+ * {@link ColorActivity} represents a user interface with {@link MaterialButton} views that allow
+ * the user to change the color of the squares of the Lights Out game.
+ */
 public class ColorActivity extends AppCompatActivity {
 
+    /**
+     * {@link String} constant used to identify the color ID in a {@link Bundle} passed between the
+     * {@link MainActivity} and {@link ColorActivity}.
+     */
     public static final String COLOR_EXTRA = "color";
 
+    /**
+     * Callback method invoked when the activity is created. First, it inflates the layout of this
+     * activity. Then, it receives the color ID used to light squares up from {@link MainActivity}.
+     * Finally, it puts the {@link MaterialButton} in the layout that represents the color ID in a
+     * selected state.
+     */
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_color);
 
-        // Get the color ID from MainActivity
+        // Get the color ID from MainActivity.
         Intent intent = getIntent();
         int colorId = intent.getIntExtra(COLOR_EXTRA, R.color.yellow);
 
-        // Select the radio button matching the color ID
-        int radioId = R.id.radio_yellow;
+        // Select the ID of the MaterialButton matching the color ID.
+        int buttonId = R.id.yellow_button;
         if (colorId == R.color.red) {
-            radioId = R.id.radio_red;
+            buttonId = R.id.red_button;
         } else if (colorId == R.color.orange) {
-            radioId = R.id.radio_orange;
+            buttonId = R.id.orange_button;
         } else if (colorId == R.color.green) {
-            radioId = R.id.radio_green;
+            buttonId = R.id.green_button;
         }
 
-        RadioButton radio = findViewById(radioId);
-        radio.setChecked(true);
+        // Put the selected MaterialButton in a selected state.
+        MaterialButton selectedButton = findViewById(buttonId);
+        selectedButton.setIcon(AppCompatResources.getDrawable(this, R.drawable.ic_radio_button_checked));
     }
 
     /**
@@ -54,16 +71,24 @@ public class ColorActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    /**
+     * Method invoked when a {@link MaterialButton} is selected in this activity.
+     *
+     * @param view {@link View} selected.
+     */
     public void onColorSelected(View view) {
+
+        // Get a color ID based on which MaterialButton was selected.
         int colorId = R.color.yellow;
-        if (view.getId() == R.id.radio_red) {
+        if (view.getId() == R.id.red_button) {
             colorId = R.color.red;
-        } else if (view.getId() == R.id.radio_orange) {
+        } else if (view.getId() == R.id.orange_button) {
             colorId = R.color.orange;
-        } else if (view.getId() == R.id.radio_green) {
+        } else if (view.getId() == R.id.green_button) {
             colorId = R.color.green;
         }
 
+        // Finish this activity and pass the color ID to MainActivity.
         Intent intent = new Intent();
         intent.putExtra(COLOR_EXTRA, colorId);
         setResult(RESULT_OK, intent);
